9597c38d967d88c3738e2a0ad60c93ff41802c7c
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-06-08 10:34:20 -0300

    adição de melhor formatação de tempo

--- cronometro/script.js
+++ cronometro/script.js
@@ -1,15 +1,36 @@
 function iniciar(){
+    let useconds  = 0, dseconds  = 0, uminutes  = 0, dminutes = 0
     if(document.getElementById('tempo').innerHTML > 0){
         parar()
     }
-    document.getElementById('tempo').innerHTML = 0
-    cronometro = setInterval(function(){
-        var tempo = document.getElementById('tempo');
-        var soma = parseInt(tempo.innerHTML) + 1
-        tempo.innerHTML = soma
-    }, 1000)
+    document.getElementById('tempo').innerHTML = `00:00`
+    contador(dminutes, uminutes, dseconds, useconds)
 }
-
+function contador(dminutes, uminutes, dseconds, useconds){
+    seconds = setInterval(() => {
+        ++useconds
+        if(useconds >= 10){
+            useconds = 0;
+            ++dseconds
+            if(dseconds >= 6){
+                dseconds = 0
+                ++uminutes
+                if(uminutes >= 10){
+                    uminutes = 0
+                    ++dminutes
+                    if(dminutes >= 6){
+                        dminutes = 0
+                        uminutes = 0
+                        dseconds = 0
+                        useconds = 0
+                    }
+                }
+            }
+        }
+        document.getElementById('tempo').innerHTML = `${dminutes}${uminutes}:${dseconds}${useconds}`
+    }, 1000)
+    
+}
 function parar(){
-    clearInterval(cronometro)
+    clearInterval(seconds)
 }
