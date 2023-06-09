546f52e9cae8768a67b12b6659bc7b2e4eb6b1c7
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-06-09 10:34:45 -0300

    correção de bug ao iniciar várias vezes e botão de pause

--- cronometro/script.js
+++ cronometro/script.js
@@ -1,7 +1,7 @@
-const botao = document.getElementById("modos");
-const corpo = document.querySelector("body")
-const darkIcon = document.getElementById("dark-icon")
-const lightIcon = document.getElementById("light-icon")
+const corpo = document.querySelector("body");
+const checkStart = document.getElementById("check-start"); 
+let botao = document.getElementById("modos"); 
+
 botao.onclick = function (){
     corpo.classList.toggle("modo-ativo")
     corpo.classList.toggle("dark-mode")
@@ -9,24 +9,26 @@ botao.onclick = function (){
 }
 
 
-
 window.onload = ()=>{
     useconds  = 0, dseconds  = 0, uminutes  = 0, dminutes = 0
 }
 
 function iniciar(){
-    parar()
-    /*if(document.getElementById("tempo").innerHTML != "00:00"){
-        parar()
-    }*/
-    useconds  = 0, dseconds  = 0, uminutes  = 0, dminutes = 0
-    document.getElementById('tempo').innerHTML = `00:00`
-    contador(dminutes, uminutes, dseconds, useconds)
-    ativo = true;
-}
-function contador(dminutes, uminutes, dseconds, useconds){
+    if(checkStart.checked){
+        return;
+    }else{
+        checkStart.checked = true;
+        contagem()
+    }
     
-     seconds = setInterval(() => {
+}
+
+function contagem(){
+
+    /*contador(dminutes, uminutes, dseconds, useconds)*/
+
+    seconds = setInterval(() => {
+        
         ++useconds
         if(useconds >= 10){
             useconds = 0;
@@ -46,16 +48,31 @@ function contador(dminutes, uminutes, dseconds, useconds){
                 }
             }
         }
+        
         document.getElementById('tempo').innerHTML = `${dminutes}${uminutes}:${dseconds}${useconds}`
     }, 1000)
-    
 }
-function parar(){
+/*function contador(dminutes, uminutes, dseconds, useconds){
+    
+}*/
+
+function pausar(){
     try{
         clearInterval(seconds)
+        console.log(seconds)
+        checkStart.checked = false;
     }
     catch{
         return;
     }
-    
+}
+
+
+
+
+function parar(){
+    pausar()
+    document.getElementById('tempo').innerHTML = `00:00`
+    useconds  = 0, dseconds  = 0, uminutes  = 0, dminutes = 0;
+    checkStart.checked = false;
 }
